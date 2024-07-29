bd9f36c108b7516ad41ea2c8088235fc919cf0c9
Beckett <133655553+severian42@users.noreply.github.com>
2024-07-29 04:31:50 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,14 +1,13 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-
 device = "cuda"
 model_id = "evol_merge"
 model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-question = "ジャネットのアヒルは1日に16個の卵を生みます。ジャネットは毎朝朝食の一環で3個を消費し、毎日4個使って友達向けにマフィンを焼きます。残りを市場で1個あたり2ドルの価格で売ります。彼女は毎日市場でいくら手に入れていますか？"
-prompt = f"次の数学の問題を解いてください。最終的な答えを出す前に、解答の推論過程を記述してください。答えには整数の答え以外何も追加しないでください。\n問題:{question}\n答え:"
+question = "Janet's duck lays 16 eggs a day. Janet consumes 3 as part of her breakfast every morning and uses 4 daily to bake muffins for her friends. She sells the rest at the market for $2 each. How much does she make at the market each day?"
+prompt = f"Please solve the following math problem. Describe the reasoning process of the solution before giving the final answer. In the answer, include only the integer answer without any additional text.\nProblem:{question}\nAnswer:"
 inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
 tokens = model.generate(
   **inputs,
